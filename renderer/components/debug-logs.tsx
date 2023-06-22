ab8bbe769c3ea3046c116b36b0df72bc66e3e93f
Dario <stastnydario@gmail.com>
2023-06-22 11:58:16 +0200

    added debug log in accordion

--- renderer/components/debug-logs.tsx
+++ renderer/components/debug-logs.tsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { Card, CardContent } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 type DebugLogsProps = {
   logs: Array<{
@@ -12,49 +20,60 @@ type DebugLogsProps = {
 
 const DebugLogs: React.FC<DebugLogsProps> = React.memo(({ logs }) => {
   return (
-    <Card
-      variant="outlined"
-      sx={{
-        margin: "20px",
-        overflow: "auto",
-        padding: "5px",
-        textAlign: "left",
-      }}
-    >
-      <CardContent>
-        {logs.map((log, index) => (
-          <div
-            key={index}
-            style={{
-              padding: "5px",
-              borderBottom: "1px solid #ccc",
-              borderRadius: "4px",
-              marginTop: "5px",
-              fontFamily: "monospace",
-            }}
-          >
-            <p style={{ margin: "1px" }}>
-              <strong>Timestamp:</strong> {log.timestamp}
-            </p>{" "}
-            <p style={{ margin: "1px" }}>
-              <strong>Type:</strong> {log.type}
-            </p>
-            <p style={{ margin: "1px" }}>
-              {" "}
-              <strong>Message:</strong> {log.msg}
-            </p>
-            <strong>Payload:</strong>
-            <ul style={{ listStyleType: "none", padding: 0, margin: 0 }}>
-              {Object.entries(log.payload ?? {}).map(([key, value]) => (
-                <li key={key}>
-                  {key}: {String(value)}
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </CardContent>
-    </Card>
+    <Accordion>
+      <AccordionSummary
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+        expandIcon={<ExpandMoreIcon />}
+      >
+        <Typography>Debug Logs</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Card
+          variant="outlined"
+          sx={{
+            margin: "20px",
+            overflow: "auto",
+            padding: "5px",
+            textAlign: "left",
+          }}
+        >
+          <CardContent>
+            {logs.map((log, index) => (
+              <div
+                key={index}
+                style={{
+                  padding: "5px",
+                  borderBottom: "1px solid #ccc",
+                  borderRadius: "4px",
+                  marginTop: "5px",
+                  fontFamily: "monospace",
+                }}
+              >
+                <p style={{ margin: "1px" }}>
+                  <strong>Timestamp:</strong> {log.timestamp}
+                </p>{" "}
+                <p style={{ margin: "1px" }}>
+                  <strong>Type:</strong> {log.type}
+                </p>
+                <p style={{ margin: "1px" }}>
+                  {" "}
+                  <strong>Message:</strong> {log.msg}
+                </p>
+                <strong>Payload:</strong>
+                <ul style={{ listStyleType: "none", padding: 0, margin: 0 }}>
+                  {Object.entries(log.payload ?? {}).map(([key, value]) => (
+                    <li key={key}>
+                      {key}: {String(value)}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </CardContent>
+        </Card>
+      </AccordionDetails>
+    </Accordion>
   );
 });
 
